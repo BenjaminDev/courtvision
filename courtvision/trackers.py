46bc77daf8eb9049c4c990614ac9f0877e01fa3c
benjamin <benjamin.decharmoy@gmail.com>
2023-06-06 21:43:45 +0200

    tracker taking shape

--- courtvision/trackers.py
+++ courtvision/trackers.py
@@ -32,6 +32,7 @@ class Tracker:
         self,
         num_particles: int = 1000,
         world_to_cam: torch.Tensor | None = None,
+        cam_to_image: torch.Tensor | None = None,
         court_size: torch.tensor = torch.tensor(
             [PadelCourt.width, PadelCourt.length, PadelCourt.backall_fence_height]
         ),
@@ -42,22 +43,29 @@ class Tracker:
             (num_particles, 3 * 3 + 1),
             names=["num_particles", "state"],
         )
-        self.states[
-            :, StateIdx.x
-        ] = 50.0  # self.states[:,StateIdx.x] * court_size[StateIdx.x] +court_size[StateIdx.x]/2
-        self.states[
-            :, StateIdx.y
-        ] = 70.0  # self.states[:,StateIdx.y] * court_size[StateIdx.y] + court_size[StateIdx.y]/2
-        self.states[
-            :, StateIdx.z
-        ] = 5.0  # self.states[:,StateIdx.z] * court_size[StateIdx.z] + court_size[StateIdx.z]/2
+        self.states[:, StateIdx.x] = (
+            self.states[:, StateIdx.x] * court_size[StateIdx.x]
+            + court_size[StateIdx.x] / 2
+        )
+        self.states[:, StateIdx.y] = (
+            self.states[:, StateIdx.y] * court_size[StateIdx.y]
+            + court_size[StateIdx.y] / 2
+        )
+        self.states[:, StateIdx.z] = (
+            self.states[:, StateIdx.z] * court_size[StateIdx.z]
+            + court_size[StateIdx.z] / 2
+        )
         self.states[:, StateIdx.weight] = torch.abs(
             self.normalized_weights(self.states)
         )
 
-        self.H = torch.randn((3, 4))
+        self.cam_to_image = torch.randn((3, 4))
+        if cam_to_image is not None:
+            self.cam_to_image = cam_to_image.to(dtype=torch.float32)
+        self.world_to_cam = torch.randn((4, 4))
         if world_to_cam is not None:
-            self.H = world_to_cam.to(dtype=torch.float32)
+            self.world_to_cam = world_to_cam.to(dtype=torch.float32)
+        # self.H = self.cam_to_image @ self.world_to_cam
 
     def set_states_to(self, point: torch.tensor):
         # tracker.states[:,0:3] = torch.tensor([1.0, 2.0, 3.0]).repeat((1000,1))
@@ -77,20 +85,34 @@ class Tracker:
         )
 
     @staticmethod
-    def state_to_observation(state, H):
+    def state_to_observation(state, *, world_to_cam, cam_to_image):
         x_y_z_1_positions = convert_points_to_homogeneous(
             state[:, : StateIdx.z + 1].rename(None)
         )
-        return convert_points_from_homogeneous((H @ x_y_z_1_positions.T).T)
+        x_y_z_1_positions_cam = convert_points_from_homogeneous(
+            x_y_z_1_positions @ world_to_cam.T
+        )
+        return convert_points_from_homogeneous(x_y_z_1_positions_cam @ cam_to_image.T)
+
+    def likelihood_in_state_space(
+        self, obs_state: torch.tensor, pred_state: torch.tensor
+    ):
+        # unproject_points
+        ...
 
     # # Define the likelihood function
     def likelihood(self, obs_state: torch.tensor, pred_state: torch.tensor):
-        sigma = 0.1  # Standard deviation of the observation noise
+        sigma = 20.1  # Standard deviation of the observation noise
         from torch.nn.functional import mse_loss
 
-        pred_obs = self.state_to_observation(pred_state, self.H)
-        mse = mse_loss(pred_obs, obs_state, reduction="none").sum(dim=1)
-        p = torch.min(torch.exp(0.5 * mse / sigma) ** 2, torch.tensor(1e-3))
+        pred_obs = self.state_to_observation(
+            pred_state, world_to_cam=self.world_to_cam, cam_to_image=self.cam_to_image
+        )
+        mse = mse_loss(pred_obs, obs_state.expand_as(pred_obs), reduction="none").sum(
+            dim=1
+        )
+        # p = torch.exp(-0.5 * mse / sigma)
+        p = torch.max(torch.exp(-0.5 * mse / sigma) ** 2, torch.tensor(1e-3))
         return p
 
     @staticmethod
