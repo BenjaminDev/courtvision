81ff56d007496f7cfb63aa25a40456fd2331451c
benjamin <benjamin.decharmoy@gmail.com>
2023-06-08 14:17:33 +0200

    adds presentation

--- courtvision/trackers.py
+++ courtvision/trackers.py
@@ -43,6 +43,7 @@ class Tracker:
             (num_particles, 3 * 3 + 1),
             names=["num_particles", "state"],
         )
+        # Place a prior on the initial state X, Y, Z to be on the court
         self.states[:, StateIdx.x] = (
             self.states[:, StateIdx.x] * court_size[StateIdx.x]
             + court_size[StateIdx.x] / 2
@@ -58,6 +59,11 @@ class Tracker:
         self.states[:, StateIdx.weight] = torch.abs(
             self.normalized_weights(self.states)
         )
+        # Set the initial velocity to be zero
+        self.states[:, StateIdx.vx : StateIdx.vz + 1] = 0.0
+        # Set the initial acceleration to be zero but -9.8 in the z direction
+        self.states[:, StateIdx.ax : StateIdx.az + 1] = 0.0
+        self.states[:, StateIdx.az] = -9.8
 
         self.cam_to_image = torch.randn((3, 4))
         if cam_to_image is not None:
@@ -77,6 +83,22 @@ class Tracker:
     def xyz(self) -> torch.tensor:
         return self.states[:, StateIdx.x : StateIdx.z + 1].clone()
 
+    @property
+    def mean_image_plane_prediction(self) -> torch.tensor:
+        return self.state_to_observation(
+            self.xyz_mean,
+            world_to_cam=self.world_to_cam,
+            cam_to_image=self.cam_to_image,
+        )
+
+    @property
+    def xyz_mean(self) -> torch.tensor:
+
+        xyz_mean = (
+            self.xyz.rename(None).T @ self.states[:, StateIdx.weight].rename(None)
+        ) / self.states[:, StateIdx.weight].sum()
+        return xyz_mean.unsqueeze(0)
+
     @staticmethod
     def normalized_weights(states: torch.tensor) -> torch.tensor:
         return (
@@ -86,6 +108,7 @@ class Tracker:
 
     @staticmethod
     def state_to_observation(state, *, world_to_cam, cam_to_image):
+
         x_y_z_1_positions = convert_points_to_homogeneous(
             state[:, : StateIdx.z + 1].rename(None)
         )
@@ -114,6 +137,53 @@ class Tracker:
         # p = torch.exp(-0.5 * mse / sigma)
         p = torch.max(torch.exp(-0.5 * mse / sigma) ** 2, torch.tensor(1e-3))
         return p
+
+    def predict(self, dt: float = 1.0 / 30.0):
+        # Define the transition model
+        # state: [x, y, z, vx, vy, zv, ax, ay, az, ax, ay, az, weight]
+
+        self.states[:, StateIdx.x : StateIdx.z + 1] = (
+            self.states[:, StateIdx.x : StateIdx.z + 1]
+            + torch.randn((self.num_particles, 3)) * 5.0
+        )
+        # Ensure state is on the court using clamp
+        self.states[:, StateIdx.x] = torch.clamp(
+            self.states[:, StateIdx.x], 0.0, PadelCourt.width
+        )
+        self.states[:, StateIdx.y] = torch.clamp(
+            self.states[:, StateIdx.y], 0.0, PadelCourt.length
+        )
+        self.states[:, StateIdx.z] = torch.clamp(
+            self.states[:, StateIdx.z], 0.0, PadelCourt.backall_fence_height * 1.5
+        )
+
+        # self.states[:, StateIdx.x : StateIdx.z + 1] += (
+        #     self.states[:, StateIdx.vx : StateIdx.vz + 1] * dt + torch.randn(
+        #         (self.num_particles, 3)
+        #     )*5.0
+        # )
+        # # Z cannot be less than zero
+        # self.states[:, StateIdx.z] = torch.max(self.states[:, StateIdx.z].rename(None), torch.tensor(0.0))
+
+        # self.states[:, StateIdx.vx : StateIdx.vz + 1] += (
+        #     self.states[:, StateIdx.ax : StateIdx.az + 1] * dt
+        # )
+        # randomize the acceleration in the x and y directions
+        # self.states[:, StateIdx.ax : StateIdx.az + 1] += torch.randn(
+        #     (self.num_particles, 3)
+        # )
+        # Set the acceleration in the z direction to be -9.8
+        # and the acceleration in the x and y directions to be zero
+        # self.states[:, StateIdx.ax : StateIdx.az + 1] = 0.0
+        # self.states[:, StateIdx.az] = -9.8
+
+    def update(self, obs_state: torch.tensor, score: torch.tensor = torch.tensor(1.0)):
+        # Define the likelihood function
+        likelihoods = self.likelihood(obs_state, self.states)
+        self.states[:, StateIdx.weight] = self.update_weights(
+            self.states[:, StateIdx.weight], likelihoods
+        )
+        self.states = self.resample(self.states, self.states[:, StateIdx.weight])
 
     @staticmethod
     def update_weights(
--- scripts/ttf.sh
+++ scripts/ttf.sh
@@ -1,1 +1,4 @@
-jupyter nbconvert /Users/benjamindecharmoy/projects/courtvision/docs/presentation/TTF.ipynb --to slides --post serve
+jupyter nbconvert /Users/benjamindecharmoy/projects/courtvision/docs/presentation/TTF.ipynb --to slides
+sed -i  's|slideNumber: "",|slideNumber: "", height:600, margin: 0.00,|'  /Users/benjamindecharmoy/projects/courtvision/docs/presentation/TTF.slides.html
+# mv /Users/benjamindecharmoy/projects/courtvision/docs/presentation/TTF.slides.html /Users/benjamindecharmoy/projects/courtvision/docs/presentation/web/TTF.slides.html
+python -m http.server -d .
