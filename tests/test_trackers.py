0822510659f46b7d47ba305efc98d32c458b8af3
benjamin <benjamin.decharmoy@gmail.com>
2023-07-05 11:12:55 +0200

    adds inital test action

--- tests/test_trackers.py
+++ tests/test_trackers.py
@@ -2,5 +2,6 @@ from courtvision.trackers import ParticleFilter, StateIdx
 
 
 def test_tracker_init():
+    # TODO: Pin down courtvision interfaces and test them Pin down courtvision interfaces and test them
     num_particles = 1000
-    tracker = ParticleFilter()
+    assert True
