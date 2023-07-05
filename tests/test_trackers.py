89ba7b9faab690585e8c16b30347288d06629606
benjamin <benjamin.decharmoy@gmail.com>
2023-07-05 11:32:07 +0200

    remove imports

--- tests/test_trackers.py
+++ tests/test_trackers.py
@@ -1,6 +1,3 @@
-from courtvision.trackers import ParticleFilter, StateIdx
-
-
 def test_tracker_init():
     # TODO: Pin down courtvision interfaces and test them Pin down courtvision interfaces and test them
     num_particles = 1000
